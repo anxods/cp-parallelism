e479230320c55c844c309e42c3203aecfc3fe91f
Anxo Díaz Sande <anxo.diaz1@udc.es>
2020-05-16 20:57:34 +0200

    Version final

--- p3/p3.c
+++ p3/p3.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <math.h>
 #include <mpi.h>
 
 #define DEBUG 1
@@ -55,9 +56,9 @@ int base_distance(int base1, int base2){
 int main(int argc, char *argv[] ) {
 
 	int i, j;
-	int *data1, *data2;
+	int *data1_aux, *data2_aux, *data1, *data2;
 	int *result;
-	struct timeval  tv1, tv2, tv1_1, tv2_1, tvi, tvf;
+	struct timeval tv1, tv2, tv1_1, tv2_1, tvi, tvf;
 
 	int numprocs, procID;
 	int *microsecondsCompTotal, *result_total, *microsecondsCommTotal;
@@ -66,7 +67,12 @@ int main(int argc, char *argv[] ) {
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &procID);
 
+	int rows = ceil((float) M / numprocs);
+	int rowsProcess;
+
+	data1_aux = (int *) malloc(M*N*sizeof(int));
 	data1 = (int *) malloc(M*N*sizeof(int));
+	data2_aux = (int *) malloc(M*N*sizeof(int));
 	data2 = (int *) malloc(M*N*sizeof(int));
 	result = (int *) malloc(M*sizeof(int));
 	microsecondsCompTotal = malloc(numprocs *sizeof(int));
@@ -77,75 +83,44 @@ int main(int argc, char *argv[] ) {
 		/* Initialize Matrices */
 		for(i=0;i<M;i++) { // enter the sequence i
 			for(j=0;j<N;j++) { // once we are inside the sequence i we check for the base j
-				data1[i*N+j] = (i+j)%5;
-				data2[i*N+j] = ((i-j)*(i-j))%5;
+				data1_aux[i*N+j] = (i+j)%5;
+				data2_aux[i*N+j] = ((i-j)*(i-j))%5;
 			}
-		}
-	}
-
-	int rowsArray[numprocs];
-	int rows = 0;
-	int assignedRows = 0;
-
-	if (procID == ROOT){
-		// We need to compute the number of rows per process
-		if ((M % numprocs) == 0){ // if M is a multiple of the numprocs
-			
-			for (i=0; i<numprocs; i++){
-				rowsArray[i] = M / numprocs; // we assign the number of
-				assignedRows += rowsArray[i]; 
-			}
-
-			/* Just to check if it was working properly
-			if (procID==0) {
-				printf("- Assigned rows: \n");
-				
-				for (i=0; i<numprocs; i++)
-					printf(" Process %d: %d \n",i,procs[i]); 
-
-				printf(" Total: %d", assignedRows); 
-				printf("\n\n");
-			}
-			*/
-
-		} else { // case it is not a multiple
-			
-			for (i=0; i<numprocs-1; i++){
-				rowsArray[i] = M / numprocs + 1; // (as if we were using a floor, but getting an integer)
-				assignedRows += rowsArray[i]; 
-			}
-
-			rowsArray[numprocs-1] = M - assignedRows; // we assign the rest to the last process
-			
-			/* Just to check if it was working properly
-			if (procID==0) {
-				printf("- Assigned rows: \n");
-				for (i=0; i<numprocs; i++)
-					printf(" Process %d: %d \n",i,procs[i]); 
-
-				printf(" Total: %d", assignedRows + procs[numprocs-1]); 
-				printf("\n\n");
-			}
-			*/
 		}
 	}
 
 	gettimeofday(&tvi, NULL);
 
-	// Once the root process got the array with the number of rows assigned to each process (including itself)
-	// we send this to all the processes, with MPI_Scatter.
-	MPI_Scatter(&rowsArray, 1, MPI_INT, &rows, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+	// To perform the base_distance computation, we firstly assign (rows)*N elements to each process
+	// from both data1_aux and data2_aux. Afterwards, we will perform the proper domain decomposition,
+	// deleting some possible "garbage" elements if necessary.
+
+	MPI_Scatter(data1_aux,rows*N,MPI_INT,data1,rows*N,MPI_INT,ROOT,MPI_COMM_WORLD);
+	MPI_Scatter(data2_aux,rows*N,MPI_INT,data2,rows*N,MPI_INT,ROOT,MPI_COMM_WORLD);
 
 	gettimeofday(&tvf, NULL);
 
 	int microsecondsScatter = (tvf.tv_usec - tvi.tv_usec)+ 1000000 * (tvf.tv_sec - tvi.tv_sec);
 
-	// Once we have assigned the rows to each process, we proceed to compute the actual 
+	// Here's where we assign the corresponding rows to each process (domain decomposition).
+	// As we said before, this part of the code is useful to eliminate some possible "garbage" data assigned
+	// before. For example, if M is not a multiple of numprocs, we will be assigning some extra data
+	// to the last process; with the following part, we ensure that, at least, this last process does not
+	// deal with any "garbage" data, and so the program works as expected without bug numbers at the output.
+
+	if (procID == numprocs-1)
+		rowsProcess =  M - rows * (numprocs - 1);
+	else
+		rowsProcess = rows;
+
+
+	// Once we have assigned the rowsProcess to each process, we proceed to compute the actual 
 	// base_distance between the sequences (also measuring the time):
 
 	gettimeofday(&tv1, NULL);
 
-	for(i = 0; i < rows; i++) { // process entering the loop deals with its own assigned rows
+	// Process entering the loop deals with its own assigned rowsProcess (by means of data1 and data2)
+	for(i = 0; i < rowsProcess; i++) { 
 		result[i]=0;
 		for(j = 0; j < N; j++) {
 			result[i] += base_distance(data1[i*N+j], data2[i*N+j]);
@@ -154,16 +129,16 @@ int main(int argc, char *argv[] ) {
 
 	gettimeofday(&tv2, NULL);
 
-	int microseconds = (tv2.tv_usec - tv1.tv_usec)+ 1000000 * (tv2.tv_sec - tv1.tv_sec);
+	int microsecondsComp = (tv2.tv_usec - tv1.tv_usec)+ 1000000 * (tv2.tv_sec - tv1.tv_sec);
 
-	MPI_Gather(&microseconds, 1, MPI_INT, microsecondsCompTotal, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+	MPI_Gather(&microsecondsComp, 1, MPI_INT, microsecondsCompTotal, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
 	// Here we end measuring the time it took to compute the base_distance, assigning to it the 
 	// value "microseconds"
 
 	gettimeofday(&tv1_1, NULL);
 	
-	MPI_Gather(result, rows, MPI_INT, result_total, rows, MPI_INT, ROOT, MPI_COMM_WORLD);	
+	MPI_Gather(result, rowsProcess, MPI_INT, result_total, rowsProcess, MPI_INT, ROOT, MPI_COMM_WORLD);	
 
 	gettimeofday(&tv2_1, NULL);
 
@@ -174,19 +149,19 @@ int main(int argc, char *argv[] ) {
 	MPI_Gather(&microsecondsComm, 1, MPI_INT, microsecondsCommTotal, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
 	if (procID == 0){
-		/*Display result */
-		if (!DEBUG){
+		/* Display result */
+		if (DEBUG){
+			
 			for(i=0;i<M;i++) {
 				printf(" %d \t ", result_total[i]);
 			}
 			printf("\n");
+
 		} else {
-			// Last task is to show the microseconds it took each process to:
-			// 1st: to compute the actual distances (microseconds)
-			// 2nd: the communication between processes, that is, the collective operations to send the values
-			// 		()
+
 			if (procID == 0){
-				printf("Times of both the computation and communication of processes (%d) used in execution:\n\n", numprocs);
+				printf("Times of both the computation and communication of processes (%d) used in execution of the base_distance program:\n\n", numprocs);
+				
 				// Computation time
 				int microseconds_comp_total = 0;
 				for(i = 0; i < numprocs; i++){
@@ -208,10 +183,12 @@ int main(int argc, char *argv[] ) {
 		}    
 	}
 
-	// One free per malloc to every value used
+	// One free per malloc to every value used (performed by the process #0)
 	if (procID == 0){
 		free(data1); 
-		free(data2); 
+		free(data2);
+		free(data1_aux);
+		free(data2_aux); 
 		free(result); 
 		free(microsecondsCompTotal); 
 		free(result_total);
